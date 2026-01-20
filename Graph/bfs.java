5dd44a290edb63783d6b006d72d33e157b73ca98
Prasad Hg <141731531+Prasad-hg@users.noreply.github.com>
2026-01-20 23:08:54 +0530

    problems update

---
+++ Graph/bfs.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class bfs {
+
+    // Edge class
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    // Create graph
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        // 1
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        // 2
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        // 3
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        // 4
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        // 5
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        // 6
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    // BFS traversal
+    public static void bfs(ArrayList<Edge>[] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[graph.length];
+
+        q.add(0);       // source
+        vis[0] = true;  // mark visited when added
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+
+            for (Edge e : graph[curr]) {
+                if (!vis[e.dest]) {
+                    q.add(e.dest);
+                    vis[e.dest] = true;
+                }
+            }
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        /*
+              1 ---- 3
+             /        \
+            0          5 ---- 6
+             \        /
+              2 ---- 4
+        */
+
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        createGraph(graph);
+        bfs(graph);
+    }
+}
